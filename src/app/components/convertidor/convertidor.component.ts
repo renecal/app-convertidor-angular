0950d67d11c7a12840c8ef2b8e5dbef75a7d2562
renecal <rene.calfin@gmail.com>
2022-06-09 09:49:31 -0400

    agregando bindings

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./convertidor.component.css']
 })
 export class ConvertidorComponent implements OnInit {
+  cantidad = 0;
+  tengo = 'USD';
+  quiero = 'EUR';
+  total = 0;
 
   constructor() { }
 
